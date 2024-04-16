da5996409496d7c1356d9bd3dda25490b80678f0
zheer barzan <zheerbarzan85@outlook.com>
2024-04-16 23:16:35 +0300

    3rd exercise

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 from datetime import date
 birth_year = input('What year were you born? ')
-
-
-
 age = date.today().year - int(birth_year)
-
 print(f'You are {age} years old.')
---
+++ exercise_3.py
@@ -0,0 +1,4 @@
+weightInLBS = float(input("Enter weight in pounds: "))
+weightInKG = weightInLBS * 0.453592
+
+print(f"Weight in KG: {weightInKG}")
