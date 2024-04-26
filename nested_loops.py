887d91cff645c285cc5be178fa0b6396803a840f
zheer barzan <zheerbarzan85@outlook.com>
2024-04-26 18:22:49 +0300

    nested loop F-word problem

--- nested_loops.py
+++ nested_loops.py
@@ -2,3 +2,13 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             print(f'({x},{y},{z})')
+
+
+
+numbers = [5, 2, 5, 2, 2]
+
+for i in numbers:
+    output = ''
+    for j in range(i):
+        output += 'x'
+    print(output)
