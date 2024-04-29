513038c90310d60a3171df12acc303565e2479a3
zheer barzan <zheerbarzan85@outlook.com>
2024-04-29 23:17:37 +0300

    diffrence between postional and keyword arguments

--- functions.py
+++ functions.py
@@ -1,5 +1,12 @@
-def greet():
-    print("Hello, World!")
 
 
-greet()
+
+def greet(first_name, last_name):
+    print(f'Hello, {first_name} {last_name}!')
+
+
+# positional arguments
+greet("zheer", "Barzan")
+
+# keyword arguments
+greet(last_name="Barzan", first_name="Zheer")
