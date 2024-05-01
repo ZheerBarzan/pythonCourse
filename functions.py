39567cf87b17fbf479745f38de668f6d87e621f3
zheer barzan <zheerbarzan85@outlook.com>
2024-05-02 00:28:25 +0300

    turinning the emoji converter into a function

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
-
+def square(number):
+    return number**2
 
 
 def greet(first_name, last_name):
@@ -10,3 +11,22 @@ greet("zheer", "Barzan")
 
 # keyword arguments
 greet(last_name="Barzan", first_name="Zheer")
+
+print(square(5))
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis ={
+        ":)": "😊",
+        ":(": "😢"
+    }
+    output =""
+
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+message = input(">")
+
+print(emoji_converter(message))
+
+
