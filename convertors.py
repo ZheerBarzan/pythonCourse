122085fe585660840e2403a3a73ff0c9a6760dc0
zheer barzan <zheerbarzan85@outlook.com>
2024-05-16 00:21:27 +0300

    modules exercise

--- convertors.py
+++ convertors.py
@@ -4,3 +4,24 @@ def kg_to_lbs(weight):
 
 def lbs_to_kg(weight):
     return weight * 0.453592
+
+
+def find_max(numbers):
+    max = numbers[0]
+    for number in numbers:
+        if number> max:
+            max = number
+    return max
+
+def find_min(numbers):
+    min = numbers[0]
+    for number in numbers:
+        if number< min:
+            min = number
+    return min
+
+def find_avg(numbers):
+    sum = 0
+    for number in numbers:
+        sum+=number
+    return sum/numbers.__len__()
--- modulesDemo.py
+++ modulesDemo.py
@@ -1,1 +1,12 @@
 import convertors
+
+print(convertors.kg_to_lbs(70))
+
+
+
+#exercise
+numbers = [10,3,6,2]
+
+print(convertors.find_max(numbers))
+print(convertors.find_min(numbers))
+print(convertors.find_avg(numbers))
